cc6d3ae5f950a224fde000ddd1041de8b10b057a
bolek117 <bolek117@users.noreply.github.com>
2022-02-23 12:51:40 +0100

    Extract KnownLetters class to separate file

--- find.py
+++ find.py
@@ -1,6 +1,7 @@
 import json
 import re
-from typing import List, Pattern, Tuple
+from typing import List, Pattern
+from known_letters import KnownLetters
 
 # TODO: Optimize guess masks by replacing removing one `?` when match (`+`) is found
 class PositionTable:
@@ -145,44 +146,6 @@ def main(lang: str, \
     print(f'INFO Included letters: {known_letters.included_letters}')
     print(f'INFO Excluded letters: {known_letters.excluded_letters}')
     print(f'INFO Found words: {len(output)}')
-
-
-class KnownLetters:
-    def __init__(self, guess_tables: Tuple[str, str]):
-        self.included_letters = []
-        self.excluded_letters = []
-        self.unknown = []
-        self.__process(guess_tables)
-
-    def __process(self, guess_tables: Tuple[str, str]) -> None:
-        for t in guess_tables:
-            word = t[0].strip().lower()
-            guess = t[1].strip()
-
-            if len(word) != len(guess):
-                raise Exception(f'Guess mask must match word length (word `{word}`)')
-
-            for i in range(len(word)):
-                letter = word[i]
-                mask = guess[i]
-
-                if mask == '-':
-                    self.__add_excluded(letter)
-                elif mask == '+' or mask == '?':
-                    self.__add_included(letter)
-                else:
-                    self.unknown.append(letter)
-
-    def __add_included(self, letter: str) -> None:
-        letter = letter[0]
-        if letter not in self.included_letters:
-            self.included_letters.append(letter)
-
-    def __add_excluded(self, letter: str) -> None:
-        letter = letter[0]
-        if letter not in self.included_letters and \
-           letter not in self.excluded_letters:
-            self.excluded_letters.append(letter)
 
 
 def __build_regex(position_tables: PositionTablesList) -> Pattern[str]:
---
+++ known_letters.py
@@ -0,0 +1,39 @@
+from typing import Tuple
+
+
+class KnownLetters:
+    def __init__(self, guess_tables: Tuple[str, str]):
+        self.included_letters = []
+        self.excluded_letters = []
+        self.unknown = []
+        self.__process(guess_tables)
+
+    def __process(self, guess_tables: Tuple[str, str]) -> None:
+        for t in guess_tables:
+            word = t[0].strip().lower()
+            guess = t[1].strip()
+
+            if len(word) != len(guess):
+                raise Exception(f'Guess mask must match word length (word `{word}`)')
+
+            for i in range(len(word)):
+                letter = word[i]
+                mask = guess[i]
+
+                if mask == '-':
+                    self.__add_excluded(letter)
+                elif mask == '+' or mask == '?':
+                    self.__add_included(letter)
+                else:
+                    self.unknown.append(letter)
+
+    def __add_included(self, letter: str) -> None:
+        letter = letter[0]
+        if letter not in self.included_letters:
+            self.included_letters.append(letter)
+
+    def __add_excluded(self, letter: str) -> None:
+        letter = letter[0]
+        if letter not in self.included_letters and \
+           letter not in self.excluded_letters:
+            self.excluded_letters.append(letter)
